79c581581c453a9752e22d391441196453db1634
olawale-o <omoogunolawale@gmail.com>
2021-08-31 07:18:15 +0100

    Add utils.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const get = async (URI) => {
-  const response =  await fetch(URI);
-  return await response.json();
+  const response = await fetch(URI);
+  return response;
 };
 
 export default get;
