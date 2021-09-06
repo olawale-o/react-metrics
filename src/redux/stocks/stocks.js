c6f25c738e82689786b34af5582ea1454a81b2ae
olawale-o <omoogunolawale@gmail.com>
2021-09-06 13:25:49 +0100

    Modify stocks.js file

--- src/redux/stocks/stocks.js
+++ src/redux/stocks/stocks.js
@@ -1,23 +1,66 @@
 import { fetchSymbols } from '../../utils/utils';
 
+// export const LOADING = 'stock/stocks/LOADING';
 export const LOAD_SYMBOLS = 'stock/stocks/LOADSTOCKS';
+export const LOAD_SYMBOL = 'stock/stocks/LOADSTOCK';
+
+// export const loading = (payload) => ({
+//   type: LOADING,
+//   payload,
+// });
 
 export const loadSymbols = (payload) => ({
   type: LOAD_SYMBOLS,
   payload,
 });
 
+export const loadSymbol = (payload) => ({
+  type: LOAD_SYMBOL,
+  payload,
+});
+
 export const getSymbols = () => (
   async function getSymbols(dispatch) {
+    let total = 0;
+    const exchangesObj = {};
+    const overall = {
+      total: 0,
+      items: [],
+    };
     const allSymbols = await fetchSymbols();
-    dispatch(loadSymbols(allSymbols));
+
+    allSymbols.forEach(({ exchangeShortName, volume, exchange }) => {
+      total += volume;
+      if (exchangeShortName in exchangesObj) {
+        exchangesObj[exchangeShortName].volume += volume;
+      } else {
+        exchangesObj[exchangeShortName] = { volume, exchange };
+      }
+    });
+
+    const items = Object.entries(exchangesObj).map(([key, value]) => (
+      {
+        id: key,
+        value,
+      }
+    ));
+    Object.assign(overall, { total, items });
+    dispatch(loadSymbols(overall));
   }
 );
 
-const symbolsReducer = (state = [], action = {}) => {
+const initialState = {
+  items: [],
+  total: 0,
+  selected: null,
+};
+
+const symbolsReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_SYMBOLS:
-      return state.concat(action.payload);
+      return action.payload;
+    case LOAD_SYMBOL:
+      return { ...state, selected: action.payload };
     default:
       return state;
   }
