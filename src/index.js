a327c035d65136f2aca9294a8839942ed0742caf
olawale-o <omoogunolawale@gmail.com>
2021-09-06 13:26:41 +0100

    Import BrowserRouter in index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/configureStore';
 
+const store = configureStore;
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root'),
 );
