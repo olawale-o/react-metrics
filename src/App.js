496290ea05d7665e1e3fd907f5b1364223c77dd2
olawale-o <omoogunolawale@gmail.com>
2021-09-06 13:49:21 +0100

    Import Detail component in App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
+import Detail from './components/Detail';
 import Main from './components/Main';
 
 const App = () => (
   <div className="App">
     <Switch>
       <Route path="/" component={Main} exact />
+      <Route path="/:id" component={Detail} exact />
     </Switch>
   </div>
 );
