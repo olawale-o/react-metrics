f633864f384a259550f655e506facfe97229e5c0
olawale-o <omoogunolawale@gmail.com>
2022-01-13 21:44:52 +0100

    Migrate to react-routerV6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Detail from './components/Detail';
 import Main from './components/Main';
 
 const App = () => (
   <div className="App">
-    <Switch>
-      <Route path="/" component={Main} exact />
-      <Route path="/:id" component={Detail} exact />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/:id" element={<Detail />} />
+    </Routes>
   </div>
 );
 
