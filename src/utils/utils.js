ebd988b88b0d2d5255bbc6de6eb0b2d35c5563f3
olawale-o <omoogunolawale@gmail.com>
2022-06-19 11:26:11 +0100

    Hide key

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 import get from '../api/api';
 
 const BASE_URI = 'https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=10000000000000&betaMoreThan=1&volumeMoreThan=100&exchange=';
-const KEY = '76109e81ecec8a4c42637f7a2cead33e';
+const KEY = process.env.REACT_APP_KEY;
 
 export const fetchSymbols = async () => {
   const EXCHANGES = ['AMEX', 'TSX', 'MUTUAL_FUND', 'EURONEXT'].join(',');
